4195794979f442cd80f4890b0a7c2a9fb3cf3392
Fury1234321 <128996006+Fury1234321@users.noreply.github.com>
2023-10-05 18:48:18 +0100

    design2project.m

--- design2project.m
+++ design2project.m
@@ -3,15 +3,15 @@ workpieceradius = 20 ;
 vc = 75 ;%in mm per min%
 ap = 1 ;%cutting depth (mm)%
 f = 0.05; %feedratw (mm/rev)%
-Kb = 190; %N/M%
-Dext = linspace(0,52e-3,1000);
+Kb = 190000000; %N/M%
+Dext =52*(10^-3); %linspace(0,52e-3,1000);
 Dint = Dext*(0.4);
 
 a = 62.5*(10^-3); %(M)%
 b = 106.5*(10^-3) ;%(M)%
-L = linspace(100e-3,250e-3,1000);
+L = 0.10;%linspace(100e-3,250e-3,1000);
 
-E = 200*(10^9);
+E = 200e9;
 
 
 %equation for finding RPM%
@@ -26,10 +26,10 @@ fc= 519*(ap^(0.8676))*(f^(0.6943));
 pc= ((fr^2)+(fc^2))^(1/2); 
 
 %deflection due to spindle bending%
-%Dbending = ((pc)/(6*E*I))*(((-(a+b)/L)*(x^3))+(1+((a+b)/L))*((x-L)^3)+((a+b)*L*x));%
+Dbending = ((pc)/(6*E*I))*(((-(a+b)/L)*(x^3))+(1+((a+b)/L))*((x-L)^3)+((a+b)*L*x))%
 x = L + a + b;
 I = (pi*((Dext).^4) - ((Dint.^4)))/64;
-Dbending = ((pc)./(6*E.*I)).*(((-(a+b)./L).*(x.^3))+(1+((a+b)./L)).*((x-L).^3)+((a+b).*L.*x));
+%Dbending = ((pc)./(6*E.*I)).*(((-(a+b)./L).*(x.^3))+(1+((a+b)./L)).*((x-L).^3)+((a+b).*L.*x));
 
 
 %I  = (pi*((Dext)^4) - ((Dint^4)))/64;%
@@ -37,18 +37,20 @@ Dbending = ((pc)./(6*E.*I)).*(((-(a+b)./L).*(x.^3))+(1+((a+b)./L)).*((x-L).^3)+(
 
 
 %reaction force at bearing B%
-Rb = pc*((a+b))./L ; %all units in meters%
+Rb = pc*((a+b))./L  %all units in meters%
 %Ra = ((Pc).*(L+a+b))./L; %all units in meters%
 
-Dbb = Rb./Kb; %all units in meters%
-Dtotal = Dbending + Dbb;
+Dbb = Rb/Kb %all units in meters%
+Dtotal = Dbending + Dbb
 
 
 
-I_graph=plot3(L,I,Dtotal);
-xlabel('Length between bearings (m)');
-ylabel('Moment of Inertia');
-zlabel('Total Deflection ');
+%I_graph=plot3(L,I,Dtotal);
+%xlabel('Length between bearings (m)');
+%ylabel('Moment of Inertia');
+%zlabel('Total Deflection ');
+%disp(I_graph)
+
 disp(I_graph)
 
 
